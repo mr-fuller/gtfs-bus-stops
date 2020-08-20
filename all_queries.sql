9ade1dd8283d92658950ea825657cf48fa51c03a
Mike Fuller <mrfuller460@gmail.com>
2020-08-20 14:39:03 -0400

    regrant privileges

--- all_queries.sql
+++ all_queries.sql
@@ -1,3 +1,6 @@
-\i "update-tables.sql"
-\i "create-routes.sql"
-\i "create-stops.sql"
+\i '/home/mike/Documents/repos/gtfs-bus-stops/update-tables.sql'
+\i '/home/mike/Documents/repos/gtfs-bus-stops/create-routes.sql'
+\i '/home/mike/Documents/repos/gtfs-bus-stops/create-stops.sql'
+
+grant all privileges on table tarta_routes to gtfsadmin;
+grant all privileges on table tarta_stops to gtfsadmin;
