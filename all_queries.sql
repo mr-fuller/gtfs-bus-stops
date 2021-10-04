f04fe1759b038c799ecdf82a5821a028b00a9b46
mrfuller <mrfuller460@gmail.com>
2021-10-04 16:24:02 -0400

    use relative file locations

--- all_queries.sql
+++ all_queries.sql
@@ -1,6 +1,6 @@
-\i '/home/mike/Documents/repos/gtfs-bus-stops/update-tables.sql'
-\i '/home/mike/Documents/repos/gtfs-bus-stops/create-routes.sql'
-\i '/home/mike/Documents/repos/gtfs-bus-stops/create-stops.sql'
+\i './update-tables.sql'
+\i './create-routes.sql'
+\i './create-stops.sql'
 
-grant all privileges on table tarta_routes to gtfsadmin;
-grant all privileges on table tarta_stops to gtfsadmin;
+grant all privileges on table tarta_routes to gtfsuser;
+grant all privileges on table tarta_stops to gtfsuser;
